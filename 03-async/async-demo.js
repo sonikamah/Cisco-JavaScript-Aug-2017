7b12627dfa31b98a34e016a390d57b9d83597dbc
Magesh <tkmagesh77@gmail.com>
2017-08-11 15:27:35 +0530

    AJAX Demos

--- 03-async/async-demo.js
+++ 03-async/async-demo.js
@@ -10,18 +10,21 @@ function getApp(){
 		var result = addSync(x,y);
 		console.log('[@Consumer] result = ', result);
 	}
-	function addAsync(x,y){
+	function addAsync(x,y, callback){
 		console.log('	[@Service] processing ', x, ' and  ', y);
 		setTimeout(function(){
 			var result = x + y;
 			console.log('	[@Service] returning result');
-			return result;
+			if (typeof callback === 'function')
+				callback(result);
 		},3000);
 	}
 	function addAsyncClient(x,y){
 		console.log('[@Consumer] triggering addAsync');
-		var result = addAsync(x,y);
-		console.log('[@Consumer] result = ', result);
+		addAsync(x,y, function(result){
+			console.log('[@Consumer] result = ', result);	
+		});
+		
 	}
 	return {
 		addSyncClient : addSyncClient,
